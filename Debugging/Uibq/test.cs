dfd5f4c85418d9972a1d793decbdcb0dc9379b13
Gudiev, Artur <Artur.Gudiev@emc.com>
2019-05-10 11:31:47 +0300

    Before splitting connection to Ports on InPort and OutPort

---
+++ Debugging/Uibq/test.cs
@@ -0,0 +1,16 @@
+﻿//Generated material. Generating code in C#.
+
+
+	public class Controller1  : Controller{ 
+
+	enum Controller1State { 
+                  State1,
+                  State2,
+          }
+    private Controller1State controllerState ; //get first state
+
+
+}
+
+
+
