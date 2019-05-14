56d6a2c960b8cb478c7c58d60d37707d7b1d10bb
Gudiev, Artur <Artur.Gudiev@emc.com>
2019-05-14 23:20:56 +0300

    Before something

--- Debugging/Uibq/Controller.cs
+++ Debugging/Uibq/Controller.cs
@@ -1,49 +1,89 @@
 ﻿//Generated material. Generating code in C#.
 
 
-	public class MainController  : Controller{ 
+public class MainController : Controller
+{
 
-	enum MainControllerState { 
-                  StartState,
-                  ShowState,
-          }
-  private MainControllerState controllerState =  MainControllerState.StartState;
+    enum MainControllerState
+    {
+        StartState,
+        ShowState,
+    }
+    private MainControllerState controllerState = MainControllerState.StartState;
 
- 	public override void action(){          
-  	switch(controllerState){
- 
-case MainControllerState.StartState: 
-runStartState();
-break;
- 
-case MainControllerState.ShowState: 
-runShowState();
-break;
+    public override void action()
+    {
+        switch (controllerState)
+        {
+
+            case MainControllerState.StartState:
+                runStartState();
+                break;
+
+            case MainControllerState.ShowState:
+                runShowState();
+                break;
+        }
+    }
+
+
+    //--------------------------------------------------
+
+    private void showChooseStationForm()
+    {
+
+        TextBlock FromLabel = 333;
+        TextBlock ToLabel = 333;
+        DropBox LeftComboBox = 333;
+        DropBox RightComboBox = 333;
+        StackPanel LabelPanel = 333;
+        StackPanel StackPanel2 = 333;
+        Button ShowButton = new Button
+        {
+            VerticalAlignment = VerticalAlignment.Center,
+            HorizontalAlignment = HorizontalAlignment.Center,
+            Background = new SolidColorBrush(Colors.Gray),
+            Foreground = new SolidColorBrush(Colors.Black),
+            Padding = new Thickness(Screen.NormalFontSize),
+            WrapContent = true,
+            Font = new Font(new FontFamily("Arial"), 0.5 * Screen.LargeFontSize),
+            Text = "SSSSSSSSSS"
+        };
+    }
+
+    private void runStartState()
+    {
+        showChooseStationForm();
+    }
+
+
+
+    //--------------------------------------------------
+
+    private void showShowScheduleForm()
+    {
+
+        TextBlock Title = 333;
+        ListBox ListBox = 333;
+        Button BackButton = new Button
+        {
+            VerticalAlignment = VerticalAlignment.Center,
+            HorizontalAlignment = HorizontalAlignment.Center,
+            Background = new SolidColorBrush(Colors.Gray),
+            Foreground = new SolidColorBrush(Colors.Black),
+            Padding = new Thickness(Screen.NormalFontSize),
+            WrapContent = true,
+            Font = new Font(new FontFamily("Arial"), 0.5 * Screen.LargeFontSize),
+            Text = "SSSSSSSSSS"
+        };
+    }
+
+    private void runShowState()
+    {
+        showShowScheduleForm();
+    }
+
+
 }
-	}
-
-	 
-//--------------------------------------------------
-	   
-				 private void showChooseStationForm(){
-		
-		    						FromLabel; //Company.MobileDSL.Label TextBlock											ToLabel; //Company.MobileDSL.Label TextBlock											LeftComboBox; //Company.MobileDSL.ComboBox DropBox											RightComboBox; //Company.MobileDSL.ComboBox DropBox											LabelPanel; //Company.MobileDSL.StackPanel StackPanel											StackPanel2; //Company.MobileDSL.StackPanel StackPanel											ShowButton; //Company.MobileDSL.Button Button						}         
-             
-    private void runStartState(){
-	showChooseStationForm();	}
-
-	
-     
-//--------------------------------------------------
-	   
-				 private void showShowScheduleForm(){
-		
-		    						Title; //Company.MobileDSL.Label TextBlock											ListBox; //Company.MobileDSL.ListBox ListBox											BackButton; //Company.MobileDSL.Button Button						}         
-             
-    private void runShowState(){
-	showShowScheduleForm();	}
-
-	
-    	}
 
 
